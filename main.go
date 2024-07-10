53742a59f435abbbdbc2450f49d02177bf53b372
ismannr <isyman.nurfauzan@binus.ac.id>
2024-07-11 02:45:23 +0700

    v1.2.7 -added cors

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"gin-crud/controller"
 	"gin-crud/initializers"
 	"gin-crud/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
+	"time"
 )
 
 func main() {
@@ -13,6 +15,16 @@ func main() {
 	initializers.DatabaseInit()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // pake url frontend
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	controller.UserController(r)
 	controller.GuestController(r)
 	controller.AdminController(r)
